14c3a29d62dd9eb9d34a15514aab07aac73a7789
zhangwq <zhangwq@taizhitech.com>
2022-09-19 19:42:36 +0800

    4. 加载图片和字体资源

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -17,6 +17,22 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            //添加 图片 支持
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            //添加 字体 支持
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./index.css"
+import imageUrl from './image/icon.jpg';
 //生成一个内容为Hello webpack !的div标签
 function component() {
     const element = document.createElement('div');
     element.innerHTML = "Hello webpack !";
-    element.classList.add("hello")
+    element.classList.add("hello");
     return element;
 }
 //将生成的div标签添加到body中去
 document.body.appendChild(component());
+
+//添加图片资源
+function imageComponent() {
+    const element = document.createElement('img');
+    element.src = imageUrl;
+    element.classList.add("image");
+    return element;
+}
+//将生成的图片标签添加到body中去
+document.body.appendChild(imageComponent());
